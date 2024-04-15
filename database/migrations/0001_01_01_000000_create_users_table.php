207c729b1653baef9e9894741770298d114b446c
TostadaRF <a.riverofabelo@gmail.com>
2024-04-15 21:45:33 +0000

    migracion modificada

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
+            $table->boolean('active')->default(true);
+            $table->boolean('verified')->default(false);
+            $table->string('auth_code')->nullable();
+            $table->boolean('admin')->default(false);
         });
 
         schema::create('cars', function (Blueprint $table){
